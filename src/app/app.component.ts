5708b6f2ea64b90265b071c303924095510b7fd8
Wisch096 <wisch.pg@outlook.com>
2023-12-04 14:41:26 -0300

    Style: alguns ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio';
+  isDarkMode: boolean = false;
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+  }
 }
