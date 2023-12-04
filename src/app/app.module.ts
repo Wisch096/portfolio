74bf68ebffb7c688ce7932fa9831defb8a3b6ffb
Wisch096 <wisch.pg@outlook.com>
2023-12-04 15:22:24 -0300

    Styles gerais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileIconComponent } from './components/profile-icon/profile-icon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileIconComponent
   ],
   imports: [
     BrowserModule,
