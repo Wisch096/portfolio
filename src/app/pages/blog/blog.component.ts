35efb8d7472bbefe617bd8528a2f309bd3fd87e2
Matheus Wisch <wisch.pg@outlook.com>
2025-02-25 12:43:06 -0300

    🔧 Fix: Transforming blog component in injectable

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
+})
+@Injectable({
+  providedIn: 'root',
 })
 export class BlogComponent {
   posts = [
@@ -22,7 +25,34 @@ export class BlogComponent {
       date: '2023-10-05', 
       image: '../assets/jababack.jpg'
     },
-
+    { 
+      id: 1, 
+      title: 'Post 1', 
+      content: 'Conteúdo do post 1...', 
+      date: '2023-10-01', 
+      image: '../assets/jababack.jpg' 
+    },
+    { 
+      id: 2, 
+      title: 'Post 2', 
+      content: 'Conteúdo do post 2...', 
+      date: '2023-10-05', 
+      image: '../assets/jababack.jpg'
+    },
+    { 
+      id: 1, 
+      title: 'Post 1', 
+      content: 'Conteúdo do post 1...', 
+      date: '2023-10-01', 
+      image: '../assets/jababack.jpg' 
+    },
+    { 
+      id: 2, 
+      title: 'Post 2', 
+      content: 'Conteúdo do post 2...', 
+      date: '2023-10-05', 
+      image: '../assets/jababack.jpg'
+    }
   ];
 
   constructor(private router: Router) {}
@@ -30,4 +60,5 @@ export class BlogComponent {
   viewPostDetails(postId: number) {
     this.router.navigate(['/blog', postId]);
   }
+
 }
--- src/app/pages/post-details/post-details.component.ts
+++ src/app/pages/post-details/post-details.component.ts
@@ -13,7 +13,8 @@ export class PostDetailsComponent implements OnInit {
   constructor(private route: ActivatedRoute, private blogComponent: BlogComponent) {}
 
   ngOnInit(): void {
-    const postId = +this.route.snapshot.paramMap.get('id')!;
+    let postId = +this.route.snapshot.paramMap.get('id')!;
     this.post = this.blogComponent.posts.find(p => p.id === postId);
+    console.log("conteudo do post", this.post);
   }
 }
